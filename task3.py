40f3bc8a8955d11db6d5763471be3f107dd9dd95
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-02-26 19:20:41 +0100

    added def rabin_karp

--- task3.py
+++ task3.py
@@ -51,3 +51,55 @@ def compute_prefix_function(pattern):
         pi[q] = k
     return pi
 
+# Реалізація алгоритму Рабіна-Карпа
+def rabin_karp(text, pattern):
+    comparisons = 0
+    d = 256  # Розмір алфавіту ASCII
+    q = 101  # Просте число
+    n = len(text)
+    m = len(pattern)
+    p = 0
+    t = 0
+    h = 1
+
+    for i in range(m - 1):
+        h = (h * d) % q
+
+    for i in range(m):
+        p = (d * p + ord(pattern[i])) % q
+        t = (d * t + ord(text[i])) % q
+
+    for i in range(n - m + 1):
+        if p == t:
+            for j in range(m):
+                if text[i + j] != pattern[j]:
+                    break
+                comparisons += 1
+            else:
+                return comparisons, i
+        if i < n - m:
+            t = (d * (t - ord(text[i]) * h) + ord(text[i + m])) % q
+            if t < 0:
+                t += q
+    return comparisons, -1
+
+# Заготовковий код для вимірювання часу виконання
+with open('text1.txt', 'r', encoding='latin1') as file:
+    text1 = file.read()
+with open('text2.txt', 'r', encoding='latin1') as file:
+    text2 = file.read()
+
+pattern1 = "ipsum"  # Паттерн для першого тексту
+pattern2 = "habitat"  # Паттерн для другого тексту
+
+print("Boyer-Moore:")
+print("Pattern 1:", boyer_moore(text1, pattern1))
+print("Pattern 2:", boyer_moore(text2, pattern2))
+
+print("Knuth-Morris-Pratt:")
+print("Pattern 1:", knuth_morris_pratt(text1, pattern1))
+print("Pattern 2:", knuth_morris_pratt(text2, pattern2))
+
+print("Rabin-Karp:")
+print("Pattern 1:", rabin_karp(text1, pattern1))
+print("Pattern 2:", rabin_karp(text2, pattern2))
