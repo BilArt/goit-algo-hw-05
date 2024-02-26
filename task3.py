d39fbe8da2a4c42be9ef0eba11a96181ebe516e5
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-02-26 19:16:34 +0100

    added def knuth_morris_pratt

--- task3.py
+++ task3.py
@@ -20,3 +20,34 @@ def boyer_moore(text, pattern):
             i += max(1, j - pattern.rfind(text[i + j]))
 
     return comparisons, -1  # Підрядок не знайдено
+
+# Реалізація алгоритму Кнута-Морріса-Пратта
+def knuth_morris_pratt(text, pattern):
+    comparisons = 0
+    m = len(pattern)
+    n = len(text)
+    pi = compute_prefix_function(pattern)
+    q = 0
+    for i in range(n):
+        while q > 0 and pattern[q] != text[i]:
+            q = pi[q - 1]
+            comparisons += 1
+        if pattern[q] == text[i]:
+            q += 1
+            comparisons += 1
+        if q == m:
+            return comparisons, i - m + 1
+    return comparisons, -1
+
+def compute_prefix_function(pattern):
+    m = len(pattern)
+    pi = [0] * m
+    k = 0
+    for q in range(1, m):
+        while k > 0 and pattern[k] != pattern[q]:
+            k = pi[k - 1]
+        if pattern[k] == pattern[q]:
+            k += 1
+        pi[q] = k
+    return pi
+
