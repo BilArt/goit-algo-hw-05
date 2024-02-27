0edc5a432028943f59e996abfd9de03a17fed7e7
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-02-27 18:48:16 +0100

    updated task1.py

--- task1.py
+++ task1.py
@@ -1,30 +1,37 @@
 class HashTable:
     def __init__(self):
         self.size = 10
-        self.hash_map = [[] for _ in range(self.size)]
+        self.table = [[] for _ in range(self.size)]
 
-    def _hash_function(self, key):
+    def hash_function(self, key):
         return key % self.size
 
-    def add(self, key, value):
-        hash_key = self._hash_function(key)
-        for i, (k, v) in enumerate(self.hash_map[hash_key]):
-            if k == key:
-                self.hash_map[hash_key][i] = (key, value)
-                return
-        self.hash_map[hash_key].append((key, value))
-
-    def get(self, key):
-        hash_key = self._hash_function(key)
-        for k, v in self.hash_map[hash_key]:
-            if k == key:
-                return v
-        raise KeyError(f"Key {key} not found")
+    def insert(self, key, value):
+        index = self.hash_function(key)
+        self.table[index].append((key, value))
 
     def delete(self, key):
-        hash_key = self._hash_function(key)
-        for i, (k, v) in enumerate(self.hash_map[hash_key]):
-            if k == key:
-                del self.hash_map[hash_key][i]
+        index = self.hash_function(key)
+        for i, item in enumerate(self.table[index]):
+            if item[0] == key:
+                del self.table[index][i]
                 return
-        raise KeyError(f"Key {key} not found")
+        raise KeyError("Key not found in the table")
+
+# Екземпляр таблиці хешу
+hash_table = HashTable()
+
+# Додаємо декілька елементів у таблицю хешу
+hash_table.insert(10, "A")
+hash_table.insert(20, "B")
+hash_table.insert(30, "C")
+hash_table.insert(25, "D")
+
+# Виводимо вміст таблиці хешу до видалення
+print("До видалення:", hash_table.table)
+
+# Видаляємо елемент з ключем 20
+hash_table.delete(20)
+
+# Виводимо вміст таблиці хешу після видалення
+print("Після видалення:", hash_table.table)
